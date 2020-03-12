823c8ac349ef8e6638f62ad1f4971102f8b7caea
kurra <sarsukurra11@gmail.com>
2020-03-11 20:15:41 -0400

    Login Page

---
+++ client/src/views/Signin.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="Signin">
+    <h1>This is an Sign in page</h1>
+    <form class="sign-in" action="#">
+                <h2>Sign In</h2>
+                <div>Use your account</div>
+                Email-ID: <br>
+                <input type="email" placeholder="Email" />
+                <br>
+                <br>
+                Password: <br>
+                <input type="password" placeholder="Password" />
+                <br>
+                <br>
+                <a href="#">Forgot your password?</a>
+                <br>
+                <br>
+                <button>Sign In</button>
+            </form>
+  </div>
+</template>
